1e49759c7e93197a5d204f97762027a45d18eb38
Arief <ariefnhidayah@gmail.com>
2021-06-04 14:41:00 +0700

    closure 1

---
+++ closure/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Closure adalah sebuah fungsi yang bisa disimpan dalam variabel. Dengan menerapkan konsep tersebut, kita bisa membuat fungsi didalam fungsi, atau bahkan membuat fungsi yang mengembalikan fungsi.
+
+	// closure disimpan sebagai variabel
+	var getMinMax = func(numbers []int) (int, int) {
+		var min, max int
+
+		for index, number := range(numbers) {
+			switch {
+			case index == 0:
+				max, min = number, number
+			case number > max:
+				max = number
+			case number < min:
+				min = number
+			}
+		}
+
+		return min, max
+	}
+
+	var numbers = []int {2, 3, 0, 4, 3, 2, 0, 4, 2, 0, 3}
+
+	var min, max = getMinMax(numbers)
+
+	fmt.Println("Data: ", numbers)
+	fmt.Println("Max: ", max)
+	fmt.Println("Min: ", min)
+
+	// Immediately-Invoked Function Expression (IIFE)
+	// Closure jenis ini dieksekusi langsung pada saat deklarasinya.
+	var newNumbers = func(min int) []int {
+		var temp []int
+		for _, number := range(numbers) {
+			if number < min {
+				continue
+			}
+			temp = append(temp, number)
+		}
+
+		return temp
+	}(3)
+
+	fmt.Println("original number :", numbers)
+    fmt.Println("filtered number :", newNumbers)
+	
+}
